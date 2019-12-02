74c98fedec12db1a1d6473f9126b2202192e8233
Jet Hu <774372605@qq.com>
2019-12-02 21:33:28 +0800

    update cube

--- src/App.vue
+++ src/App.vue
@@ -11,20 +11,26 @@ let camera;
 let renderer;
 let scene;
 let cube;
-
-// controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
-// controls.dampingFactor = 0.05;
-// controls.screenSpacePanning = false;
 export default {
   methods: {
     init: function () {
       container = document.querySelector( '#app' );
+      this.setScene();
+      this.setCamera();
+      this.setMaterial();
+      this.createLights();
+      this.setRender();
+    },
+    setScene: function () {
       scene = new THREE.Scene();
-      //Does not work by setting background for scene
 			scene.background = new THREE.Color( 'skyblue' );
-
-      camera = new THREE.PerspectiveCamera( 35, container.clientWidth / container.clientWidth, 0.1, 100 );
-      camera.position.set( -4, 4, 10 );
+    },
+    setCamera: function () {
+      camera = new THREE.PerspectiveCamera( 35, container.clientWidth / container.clientHeight, 0.1, 100 );
+      camera.position.set( 0, 0, 10 );
+    },
+    setMaterial: function () {
+      //Does not work by setting background for scene
       const geometry = new THREE.BoxBufferGeometry( 2, 2, 2 );
       const textureLoader = new THREE.TextureLoader();
       const texture = textureLoader.load( 'https://images.unsplash.com/photo-1515387784663-e2e29a23f69e?ixlib=rb-1.2.1&w=1000&q=80');
@@ -35,19 +41,6 @@ export default {
       } );
       cube = new THREE.Mesh( geometry, material );
       scene.add( cube );
-      this.createLights();
-      renderer = new THREE.WebGLRenderer({antialias: true});
-      renderer.setSize( container.clientWidth, container.clientHeight );
-      renderer.setPixelRatio( window.devicePixelRatio );
-      renderer.gammaFactor = 2.2;
-      renderer.gammaOutput = true;
-      renderer.physicallyCorrectLights = true;
-      container.appendChild( renderer.domElement );
-      renderer.setAnimationLoop(() => {
-        this.update();
-        this.render();
-      });
-      // this.animate()
     },
     createLights: function () {
       const ambientLight = new THREE.HemisphereLight(
@@ -61,25 +54,41 @@ export default {
 
       scene.add( ambientLight, mainLight );
     },
+    setRender: function () {
+      renderer = new THREE.WebGLRenderer({antialias: true});
+      renderer.setSize( container.clientWidth, container.clientHeight );
+      renderer.setPixelRatio( window.devicePixelRatio );
+      renderer.gammaFactor = 2.2;
+      renderer.gammaOutput = true;
+      renderer.physicallyCorrectLights = true;
+      container.appendChild( renderer.domElement );
+      renderer.setAnimationLoop(() => {
+        this.update();
+        this.render();
+      });
+    },
     update: function () {
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
-      // cube.rotation.z += 0.01;
+      cube.rotation.z += 0.01;
     },
     render: function () {
-        renderer.render( scene, camera );
+      renderer.render( scene, camera );
     },
     onWindowResize: function () {
-        camera.aspect = container.clientWidth / container.clientHeight;
-        camera.updateProjectionMatrix();
-        renderer.setSize( container.clientWidth, container.clientHeight );
+      camera.aspect = container.clientWidth / container.clientHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize( container.clientWidth, container.clientHeight );
     }
 
   },
   mounted: function () {
-      this.init();
-      window.addEventListener( 'resize', this.onWindowResize );
-      new OrbitControls(camera, renderer.domElement);
+    this.init();
+    window.addEventListener( 'resize', this.onWindowResize );
+    new OrbitControls(camera, renderer.domElement);
+//       controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+// controls.dampingFactor = 1;
+// controls.screenSpacePanning = false;
   }
 }
 </script>
